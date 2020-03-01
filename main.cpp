ce6dbdeeee9744cbe0ba75becc1eb0a4810b2587
Ann <karaicheva@phys.vsu.ru>
2020-03-01 20:40:06 +0300

    Linear Search

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+int LinearSearch(int value, const int *array) {
+    for (int i = 0; i < 10; i++) {
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
-    std :: cout << "Hello, World!" << std :: endl;
+    int numbers[10] = {1,4,5,2,3,7,8,1,0,12};
+    int searchNumber;
+    std :: cout << "enter number: ";
+    std :: cin >> searchNumber;
+    int result = LinearSearch(searchNumber, numbers);
+
+    std :: cout << result << std :: endl;
     return 0;
 }
